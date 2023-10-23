14fd36c88fa356ed2512ccb8f0135986d4bfea76
AndrewGPSilva <125920218+AndrewGPSilva@users.noreply.github.com>
2023-10-23 19:45:41 +0000

    Up

--- src/components/Home/Destaque.vue
+++ src/components/Home/Destaque.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="p-5 bg-black" id="container">
-    <div class="border-2 p-1 border-gray-700 rounded-xl bg-orange-400">
-    <div v-for="noticia in noticiasData" :key="noticia.id" class="border border-gray-200 rounded-xl p-3 bg-black text-center">
+    <div class="border-2 p-1 border-gray-700 rounded-xl bg-black">
+    <div v-for="noticia in noticiasData" :key="noticia.id" class="border border-gray-700 rounded-xl p-3 bg-black text-center">
       <img class="mb-2" :src="noticia.link" alt="Thumb do Post" />
       <Line />
       <p class="text-white text-start mt-2">{{ noticia.descricao }}</p>
