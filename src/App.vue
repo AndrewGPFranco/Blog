c1473a59a7d51a704e256bcb8f6a3d90c021c66c
AndrewGPSilva <andrewgomes1328@gmail.com>
2023-10-23 12:32:34 +0000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <Home />
+</template>
+
+<script setup>
+  import Home from "./components/Home.vue"
+  const components = {
+    Home
+  }
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1 class="text-orange-400">Bem vindo ao Blog!</h1>
+    <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+</template>
+
+<script setup>
+  const name = "Home"
+</script>
