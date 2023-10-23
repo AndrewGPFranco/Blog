352d4a0a59c7dc2ea41066eab9f3f16d7597f79d
AndrewGPSilva <125920218+AndrewGPSilva@users.noreply.github.com>
2023-10-23 15:33:19 +0000

    Footer finalizado até o momento.

---
+++ src/components/Rodape.vue
@@ -0,0 +1,25 @@
+<template>
+  <footer class="bg-black text-orange-500 text-center p-2.5">
+    <h1>&copy; {{ year }} All Rights Reserved</h1>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Rodape",
+  data() {
+    return {
+      year: ""
+    }
+  },
+  created() {
+    this.getYear()
+  },
+  methods: {
+    getYear() {
+      const currentDate = new Date()
+      this.year = currentDate.getFullYear()
+    }
+  }
+}
+</script>
--- src/components/Views/Home.vue
+++ src/components/Views/Home.vue
@@ -3,10 +3,12 @@
     <main>
       <h1 class="text-orange-500">Main</h1>
     </main>
+    <Rodape />
 </template>
 
 <script setup>
   import Navbar from "../Navbar.vue";
+  import Rodape from "../Rodape.vue";
   const name = "Home"
-  const components = { Navbar }
+  const components = { Navbar, Rodape }
 </script>
