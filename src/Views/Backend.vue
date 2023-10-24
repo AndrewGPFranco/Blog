1ca2a15a4bc50182d63f400434de4d8630e64595
AndrewGPSilva <125920218+AndrewGPSilva@users.noreply.github.com>
2023-10-24 19:14:07 +0000

    Up

--- src/Views/Backend.vue
+++ src/Views/Backend.vue
@@ -5,8 +5,11 @@
   <main>
     <Titulo titulo="Aulas sobre Laravel"/>
      <div v-for="video in videos" :key="video.id" class="-mt-4">
-      <Iframe :link="video.link" :titulo="video.titulo" :descricao="video.descricao"/>
+      <Iframe :link="video.link" :titulo="video.titulo" :descricao="video.descricao" :assistir="assistir"/>
     </div>
+    <div class="bg-black flex justify-center">
+    <button @click="assistirVideo" class="bg-black text-blue-300">Assistir</button>
+      </div>
 </main>
   <footer>
     <Rodape />
@@ -29,8 +32,14 @@ import Navbar from "../components/Navbar.vue";
     },
     data() {
     return {
-      videos: videos
+      videos: videos,
+      assistir: false
     };
+    },
+    methods: {
+      assistirVideo() {
+        this.assistir = !this.assistir
+      }
     }
   }
 </script>
--- src/components/Iframe.vue
+++ src/components/Iframe.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg-black p-6 flex flex-col justify-center text-white text-center">
     <h1 class="text-orange-500">{{ titulo }}</h1>
-    <iframe :src="link" frameborder="0" allowfullscreen></iframe>
+    <iframe v-if="assistir" :src="link" frameborder="0" allowfullscreen></iframe>
     <p class="tect-gray-300">{{ descricao }}</p>
   </div>
 </template>
@@ -12,7 +12,8 @@
     props: {
       link: String,
       titulo: String,
-      descricao: String
+      descricao: String,
+      assistir: Boolean
     }
   }
 </script>
