fe5bd9a1befe366a1cd852a04d14af61fca2f624
AndrewGPSilva <125920218+AndrewGPSilva@users.noreply.github.com>
2023-10-26 11:37:24 +0000

    Imagem de Destaque trocada

--- src/components/Home/Destaque.vue
+++ src/components/Home/Destaque.vue
@@ -1,8 +1,8 @@
 <template>
   <section class="p-5 bg-black" id="container">
-    <div class="border-2 p-1 border-gray-700 rounded-xl bg-black">
+    <div class="border-2 p-0.5 border-gray-700 rounded-xl bg-black">
       <div v-for="noticia in noticiasData" :key="noticia.id" class="border border-gray-700 rounded-xl p-3 bg-black text-center">
-        <img class="mb-2" :src="noticia.link" alt="Thumb do Post" />
+        <img class="mb-2 rounded-xl" src="../../assets/vue.jpg" alt="Thumb do Post" />
         <Line />
         <p class="text-white text-start mt-2">{{ noticia.descricao }}</p>
           <div class="flex justify-end">
