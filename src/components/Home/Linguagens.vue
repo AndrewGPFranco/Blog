fdb9bf3e8b32e266fc8a1ecb3ed4cb449305d79b
AndrewGPSilva <125920218+AndrewGPSilva@users.noreply.github.com>
2023-10-27 16:02:21 +0000

    Tecnologias logos

--- src/components/Home/Linguagens.vue
+++ src/components/Home/Linguagens.vue
@@ -1,26 +1,22 @@
 <template>
   <section class="bg-black p-3">
     <Titulo titulo="Tecnologias p/ Aprender"/>
-    <div class="flex flex-wrap justify-center">
-        <img class="w-20 m-1 h-20" v-for="(imagem, i) in imagens" :key="i" :src="imagem.src" alt="logo da linguagem" />
+    <div class="flex flex-wrap justify-center mt-1">
+        <img class="w-20 m-1 h-20" v-for="image in linguagens" :key="image.id" :src="image.imagem" alt="logo da image.nome" />
     </div>
   </section>
 </template>
 
 <script>
   import Titulo from "../Titulo.vue"
+  import linguagens from "./linguagens.json"
   export default {
     name: "Linguagens",
     components: { Titulo },
     data() {
-    return {
-      imagens: [
-        { src: 'src/assets/ruby.png' },
-        { src: 'src/assets/java.png' },
-        { src: 'src/assets/python.png' },
-        { src: 'src/assets/tailwind.png' },
-      ],
+      return {
+        linguagens
+      }
     }
-  }
   }
 </script>
