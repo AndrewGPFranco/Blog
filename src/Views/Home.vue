82318edc7402d0e994ce2ced784bc65e00788f60
Andrew Gomes <andrewgomes1328@gmail.com>
2024-01-29 21:29:52 -0300

    utilizando container docker

--- src/Views/Home.vue
+++ src/Views/Home.vue
@@ -12,25 +12,16 @@
         Toque para Assistir
       </p>
       <div class="flex justify-center text-center">
-        <button
-          @click="video"
-          v-if="assistir === false"
-          class="text-blue-500 font-bold p-1 w-2/5 mt-2 text-xl rounded-md"
-        >
+        <button @click="video" v-if="assistir === false"
+          class="text-blue-500 font-bold p-1 w-2/5 mt-2 text-xl rounded-md">
           <i class="pi pi-eye" style="font-size: 5rem; color: orange"></i>
         </button>
       </div>
       <div class="flex justify-center">
-        <iframe
-          v-if="assistir"
-          class="4/4 rounded-md lg:h-96 lg:w-3/4"
-          height="250"
-          src="https://www.youtube.com/embed/uK_Jet4nOP0?si=46DNoAVn_Tm6b3fZ"
-          title="YouTube video player"
-          frameborder="0"
+        <iframe v-if="assistir" class="4/4 rounded-md lg:h-96 lg:w-3/4" height="250"
+          src="https://www.youtube.com/embed/uK_Jet4nOP0?si=46DNoAVn_Tm6b3fZ" title="YouTube video player" frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowfullscreen
-        ></iframe>
+          allowfullscreen></iframe>
       </div>
     </div>
     <Titulo titulo="Categorias" class="-mt-6" />
@@ -79,7 +70,7 @@ export default {
 </script>
 
 <style scoped>
-  main {
-    background-color: black;
-  }
+main {
+  background-color: black;
+}
 </style>
