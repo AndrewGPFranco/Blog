7fa8a224dd4ffd7d14293ceae78ad95c144570b9
AndrewGPSilva <125920218+AndrewGPSilva@users.noreply.github.com>
2024-01-30 11:31:40 +0000

    Deixando as imagens das tecnologias dinâmicas, fazendo aparecer o nome abaixo da linguagem ao ser clicada a imagem

--- src/components/Home/Linguagens.vue
+++ src/components/Home/Linguagens.vue
@@ -2,8 +2,12 @@
   <section class="bg-black p-3">
     <Titulo titulo="Tecnologias p/ Aprender" />
     <div class="flex flex-wrap justify-center mt-1">
-      <img class="w-20 m-1 h-20" v-for="image in linguagens" :key="image.id" :src="image.imagem"
-        alt="logo da image.nome" />
+      <div class="text-center" v-for="image in linguagens" :key="image.id">
+        <img @click="exibir(image.id)" class="w-20 m-1 h-20" :src="image.imagem" alt="Logo da {{ image.nome }}" />
+        <p v-if="exibirNome === image.id" class="text-orange-400 font-bold">
+          {{ image.nome }}
+        </p>
+      </div>
     </div>
   </section>
 </template>
@@ -18,7 +22,13 @@ export default {
   data() {
     return {
       linguagens,
+      exibirNome: null
     };
+  },
+  methods: {
+    exibir(id) {
+      this.exibirNome = this.exibirNome === id ? null : id;
+    }
   }
 };
 </script>
